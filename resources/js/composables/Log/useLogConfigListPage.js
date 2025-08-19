8b0de6004fee4a1c47fdd0483adfb108231dd7fa
Yan Brasiliano Silva Penalva <yanpenabr@gmail.com>
2025-08-19 17:32:15 -0300

    refactor: simplify onConsult method in useLogConfigListPage and remove unused ACTION_VIEW constant

--- resources/js/composables/Log/useLogConfigListPage.js
+++ resources/js/composables/Log/useLogConfigListPage.js
@@ -3,7 +3,6 @@ import { LOG_PERMISSION } from '@utils/permissions';
 import { ref } from 'vue';
 
 export default function useLogConfigListPage() {
-  const ACTION_VIEW = 'view';
   const columns = ref([
     {
       name: 'description',
@@ -39,10 +38,8 @@ export default function useLogConfigListPage() {
       align: 'center',
       field: 'id',
       methods: {
-        onConsult: (row) => {
-          const hasPermissionValue = hasPermission([LOG_PERMISSION.LIST]);
-          const isNotView = row.event !== ACTION_VIEW;
-          return hasPermissionValue && isNotView;
+        onConsult: () => {
+          hasPermission([LOG_PERMISSION.LIST]);
         },
       },
     },
