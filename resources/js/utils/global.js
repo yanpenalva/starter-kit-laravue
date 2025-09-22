f9c6527242935251914dd65b71434e7ac3e6bd3c
Yan Brasiliano Silva Penalva <yanpenabr@gmail.com>
2025-09-22 18:17:40 -0300

    chore: update app version handling to support multiple environments

--- resources/js/utils/global.js
+++ resources/js/utils/global.js
@@ -1,5 +1,13 @@
 const useGlobal = () => {
-  const version = '1.0.0';
+  const versions = new Map([
+    ['local', '2.0.0'],
+    ['development', '2.0.0'],
+    ['staging', '2.0.0'],
+    ['production', '2.0.0'],
+  ]);
+
+  const env = import.meta.env.VITE_APP_ENV;
+  const version = versions.get(env) ?? '0.0.0';
 
   return { appVersion: `v ${version}` };
 };
